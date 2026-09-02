d59209027dafc2b65e02764081418558c06164ee
Ciel-noob <indrasaputrasaputra98@gmail.com>
2026-09-02 21:32:51 +0800

    Add files via upload

--- struktur_dasar.java
+++ struktur_dasar.java
@@ -1,5 +1,8 @@
 class Main {
     public static void main(String[] args) {
+        // this is my comment, I am Inevitable
+        /* like a wise man said, there is a first time for everything son */
+        
         System.out.println("Hello, World!");
         System.out.println("This is a Pen.");
     }
